a0bf1fed0209774bcd4a8d2d8878545f3424e70d
Tandra Malm <tandra_malm@hotmail.com>
2023-11-27 14:37:06 -0700

    add changes

--- express_server.js
+++ express_server.js
@@ -14,3 +14,7 @@ app.get("/", (rep, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});

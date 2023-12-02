008f196a85bbc7aa427bc225d4306d618ce84e0f
Tandra Malm <tandra_malm@hotmail.com>
2023-12-02 01:09:28 -0700

    deleting practice file

--- bcrypt-practice.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const plaintextPassword = 'monkeyFuzz';
-
-const salt = bcrypt.genSaltSync(10);
-
-console.log('salt:', salt);
-
-bcrypt.hashSync(plaintextPassword);
